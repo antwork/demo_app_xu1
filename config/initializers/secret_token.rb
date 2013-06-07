6a470234fe294379178069056d21b3a2cd1d9efb
antwork <antwork@126.com>
2013-06-08 01:12:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppXu1::Application.config.secret_token = '35405d9ef6adfb115e24a1ea35e13c3ce79532acf97b123777065ab248c73a43bf43e0fdce2359a8a3ef9eac5825fd686abd4efc17977794da6d3759f6a67807'
